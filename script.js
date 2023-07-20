20f8ac41231866a410fd8151eacdfcbd679d8b33
likes-front-end <137172921+it-tinyangel@users.noreply.github.com>
2023-07-20 20:58:45 +0300

    Finding the factorial

--- script.js
+++ script.js
@@ -3,12 +3,13 @@ let enterNumber = parseInt(prompt("Enter natural number:")); // checking whether
 if (isNaN(enterNumber)) // the function isNaN(value) checking value for being NaN
 	alert("Please, try again and enter number.") //if isNaN(value) is true, a message will be written
 else {
-if (enterNumber >= 0) {
-	let factorial = 1;
+	if (enterNumber >= 0) {
+		let factorial = 1;
 	
-	if (enterNumber > 0) // computing factorial count down from the entered number
-		for (let i = enterNumber; i > 1; i--)
-			factorial *= i;
+		if (enterNumber > 0) // computing factorial count down from the entered number
+			for (let i = enterNumber; i > 1; i--)
+				factorial *= i;
 	
-	document.write(`<h2>${enterNumber}! = ${factorial}</h2>`);
+		document.write(`<h2>${enterNumber}! = ${factorial}</h2>`);
+	}
 }
